4169e465bce5dcf851b990defd7f42b1950df43c
lynjun97 <lynjun97@naver.com>
2023-11-15 17:57:11 +0900

    Feat : 주문 기능 추가

--- src/main/java/christmas/controller/OrderController.java
+++ src/main/java/christmas/controller/OrderController.java
@@ -1,9 +1,13 @@
 package christmas.controller;
 
 import christmas.service.OrderService;
+import christmas.util.Menu;
+
+import java.util.HashMap;
 
 public class OrderController {
     private int day;
+    private HashMap<Menu,Integer> menu;
 
     public OrderController() {
         setMenu();
@@ -12,5 +16,7 @@ public class OrderController {
     private void setMenu() {
         // 방문 날짜
         day = OrderService.setDate();
+        // 주문할 메뉴와 개수
+        menu = OrderService.setMenu();
     }
 }
--- src/main/java/christmas/service/OrderService.java
+++ src/main/java/christmas/service/OrderService.java
@@ -1,10 +1,17 @@
 package christmas.service;
 
-import christmas.Date;
+import christmas.Order;
+import christmas.model.Date;
+import christmas.util.Menu;
+
+import java.util.HashMap;
 
 import static christmas.view.InputView.*;
+import static christmas.view.InputView.inputMenu;
 
 public class OrderService {
+
+    static HashMap<Menu, Integer> orderedMenu;
 
     public static int setDate() {
         String day = inputDate();
@@ -14,6 +21,18 @@ public class OrderService {
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
             return setDate();
+        }
+    }
+
+    public static HashMap<Menu, Integer> setMenu() {
+        String[] menu = inputMenu();
+        try {
+            Order order = new Order(menu);
+            orderedMenu = order.getOrder();
+            return orderedMenu;
+        } catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+            return setMenu();
         }
     }
 
