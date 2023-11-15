3e5b12d11b3fe8ea58a6f65b3bf0e19a0f326784
lynjun97 <lynjun97@naver.com>
2023-11-15 17:59:41 +0900

    Feat : 메뉴 검증 로직 추가

--- src/main/java/christmas/validate/Validation.java
+++ src/main/java/christmas/validate/Validation.java
@@ -1,9 +1,73 @@
 package christmas.validate;
 
+import christmas.util.Menu;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class Validation {
     public static void validateRange(int day) {
         if (day < 1 || day > 31) {
             throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+        }
+    }
+
+    public static HashMap<Menu, Integer> validatedMenu(String[] menu) {
+        HashMap<Menu, Integer> orderMap = new HashMap<>();
+        for (String s : menu) {
+            addTOrderMap(s, orderMap);
+        }
+        duplicatedMenu(menu.length, orderMap.size());
+        validateCategory(orderMap);
+        validateOrderOverCount(orderMap);
+        return orderMap;
+    }
+
+    public static void addTOrderMap(String s, HashMap<Menu, Integer> orderMap) {
+        String[] split = s.split("-");
+
+        int quantity = validateOrderCount(split[1]);
+
+        if (quantity <= 0) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
+        orderMap.put(Menu.valueOf(split[0]), quantity);
+    }
+
+    private static int validateOrderCount(String count){
+        try {
+            return Integer.parseInt(count);
+        } catch (NumberFormatException e){
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
+    }
+
+    public static void duplicatedMenu(int menuCnt, int OrderCnt) {
+        if (menuCnt != OrderCnt) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
+    }
+
+    public static void validateCategory(HashMap<Menu, Integer> orderMap) {
+
+        List<String> category = new ArrayList<>();
+        for (Menu menu : orderMap.keySet()) {
+            category.add(menu.getCategory());
+        }
+
+        if (category.size() == 1 && category.get(0).equals("drink")) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
+    }
+
+    private static void validateOrderOverCount(HashMap<Menu, Integer> orderMap) {
+        int sum = 0;
+        for (Integer value : orderMap.values()) {
+            sum += value;
+            if (sum > 20) {
+                throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+            }
         }
     }
 }
