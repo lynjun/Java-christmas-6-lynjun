5d8931c95665167ab5719aaaf61fa573515f3604
lynjun97 <lynjun97@naver.com>
2023-11-15 18:48:07 +0900

    Feat : 디데이 할인 계산 로직 추가

--- src/main/java/christmas/service/DiscountService.java
+++ src/main/java/christmas/service/DiscountService.java
@@ -3,6 +3,8 @@ package christmas.service;
 import christmas.model.Date;
 import christmas.view.OutputView;
 
+import static christmas.view.OutputView.*;
+
 public class DiscountService {
 
     public static void previewTheBenefits(int day){
@@ -10,5 +12,15 @@ public class DiscountService {
         String weekly = date.getWeekly();
         String dayOfTheWeek = date.getDayOfTheWeek();
         OutputView.printBenefit(day,weekly,dayOfTheWeek);
+    }
+
+    public static int dayDiscount(int day) {
+        int dayDiscount = 0;
+        if (day >= 1 && day <= 25) {
+            dayDiscount = (day - 1) * 100 + 1000;
+        }
+
+        printDdayDiscount(dayDiscount);
+        return dayDiscount;
     }
 }
