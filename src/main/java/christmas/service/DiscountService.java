eb3260c6fb60e24c7596665a636f6870a30e0847
lynjun97 <lynjun97@naver.com>
2023-11-15 18:54:04 +0900

    Feat : 평일 할인 or 주말 할인 로직 추가

--- src/main/java/christmas/service/DiscountService.java
+++ src/main/java/christmas/service/DiscountService.java
@@ -1,7 +1,10 @@
 package christmas.service;
 
 import christmas.model.Date;
+import christmas.util.Menu;
 import christmas.view.OutputView;
+
+import java.util.HashMap;
 
 import static christmas.view.OutputView.*;
 
@@ -22,5 +25,43 @@ public class DiscountService {
 
         printDdayDiscount(dayDiscount);
         return dayDiscount;
+    }
+
+    public static int weeklyDiscount(HashMap<Menu, Integer> order, int day) {
+        Date date = new Date(day);
+
+        String dayCategory = date.getWeekly();
+        String menuCategory = getTargetCategory(dayCategory);
+
+        int dessertCount = countMenuByCategory(order, menuCategory);
+        int weeklyDiscount = dessertCount * 2023;
+
+        if (weeklyDiscount > 0) {
+            OutputView.printDayOfWeekDiscount(weeklyDiscount, dayCategory);
+        }
+
+        return weeklyDiscount;
+    }
+
+    public static String getTargetCategory(String dayCategory) {
+        if (dayCategory.equals("weekday")) {
+            return "dessert";
+        }
+        if (dayCategory.equals("weekend")) {
+            return "main";
+        }
+        return "";
+    }
+
+    private static int countMenuByCategory(HashMap<Menu, Integer> order, String category) {
+        int dessertCount = 0;
+        for (Menu menu : order.keySet()) {
+            String menuCategory = menu.getCategory();
+            if (menuCategory.equals(category)) {
+                int quantity = order.get(menu);
+                dessertCount += quantity;
+            }
+        }
+        return dessertCount;
     }
 }
